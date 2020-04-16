ef7464ff776229f612c193c58cbe05b6a53bf20f
Ingmar van der Geest <ingmarvdg@gmail.com>
2020-04-17 00:18:12 +0900

    Some big changes
    The model is now using PyTorch instead of Keras with a custom model class. Grouped the functions in the utilities file into classes. Added a TSNE visualization.

--- nlp_fmri_utils.py
+++ nlp_fmri_utils.py
@@ -1,173 +1,247 @@
 import pandas as pd
-
 import re
-from typing import Dict, List
-from keras.preprocessing.sequence import pad_sequences
 import numpy as np
+import torch
+import torch.nn as nn
 
-from keras.layers import LSTM, Dense, Masking
-from keras.models import Sequential
-from keras import backend as K
+from typing import List
+from torch.utils.data import Dataset, DataLoader, random_split
+from os import path
 
-def pre_process_stories(data_path: str, n_stories: int, p_size_lim: int, content_row_name: str) -> pd.DataFrame:
-    processed_data = pd.DataFrame()
+class StoriesDataset(Dataset):
+    def __init__(self, data_path, word_embedding):
+        self.data_path = data_path
 
-    story_data = pd.read_csv(data_path, delimiter=',', nrows = n_stories)
+        if path.exists(data_path):
+            self.dataset = pd.read_csv(data_path)
 
-    for index, row in story_data.iterrows():
-        story = row[content_row_name]  # get the content of the story
-        split_story = pd.DataFrame(story.split("\n\n\n"))  # split the story by paragraph
+        self.word_embedding = word_embedding
 
-        split_story[0] = split_story[0].map(lambda x: x.strip('\n').lower())  # remove all the newlines in the text
-        split_story[0] = split_story[0].map(lambda x: re.sub(r"\W+", ' ', x).strip("  "))  # remove all triple spaces and all non words
-        split_story[0] = split_story[0].str.split()  # split each paragraph up into a list of words
-        split_story = split_story[split_story[0].apply(lambda x: len(x) > p_size_lim)]  # remove all paragraphs that are too short
+        return
 
-    return processed_data.append(split_story)
+    def generate_sequences(self, stories_path: str, story_header: str, n_stories: int, seq_len: int):
+        stories = pd.read_csv(stories_path, nrows=n_stories)[story_header]
 
+        stories = stories.map(lambda x: re.sub(r"\W+", ' ', x).strip("  "))  # remove all triple spaces and all non words
+        stories = stories.str.lower().str.replace('_',"").str.split()
 
-def load_word_embeddings(embeddings_path: str)-> (Dict[str, int], np.array):
-    words = []
-    i = 0
-    word2idx = {}
-    vectors = []
+        x = []
+        y = []
+        seqdata = pd.DataFrame(columns=['sequence', 'target'])
+        for index, story in stories.iteritems():
+            for i in range(0, len(story) - seq_len, 1):
+                rs = story[i:i + seq_len]
+                x.append(' '.join(rs))
+                y.append(story[i + seq_len])
 
-    with open(embeddings_path, 'rb') as f:
-        for l in f:
-            line = l.decode().split()
-            word = line[0]
-            words.append(word)
-            word2idx[word] = i
-            i += 1
-            vect = np.array(line[1:]).astype(np.float)
-            vectors.append(vect)
+        seqdata['sequence'] = x
+        seqdata['target'] = y
+        seqdata.to_csv(self.data_path)
 
-    vectors = np.asarray(vectors)
-    dictionary = word2idx
-    print('Made an embedding containing ', len(words), 'words.')
+        self.dataset = seqdata
 
-    return dictionary, vectors
+        return
 
+    def __len__(self):
+        return len(self.dataset)
 
-def vectorize(text: List, embeddings_matrix: np.array, embeddings_dict: Dict[str,int])-> np.array:
-    emb_dim = embeddings_matrix.shape[1]
-    matrix_len = len(text)
-    weights_matrix = np.zeros((matrix_len, emb_dim))
+    def __getitem__(self, idx):
+        target = self.word_embedding.get_word_vectors([self.dataset['target'][idx]])
+        sequence = self.dataset['sequence'][idx].split()
+        sequence_vects = self.word_embedding.get_word_vectors(sequence)
 
-    for i, word in enumerate(text):
-        try:
-            weights_matrix[i] = embeddings_matrix[embeddings_dict[word]]
-        except KeyError:
-            weights_matrix[i] = np.zeros((emb_dim,))
-
-    return weights_matrix
+        return sequence_vects, target
 
 
-def make_sequences(text: np.array, seq_length: int)-> (List, List):
-    x = []
-    y = []
-    for i in range(0, len(text) - seq_length, 1):
-        x.append(text[i:i + seq_length])
-        y.append(text[i + seq_length])
+class WordEmbedding():
+    def __init__(self, embeddings_path: str):
+        words = []
+        i = 0
+        word2idx = {}
+        vectors = []
 
-    return x, y
+        with open(embeddings_path, 'rb') as f:
+            for l in f:
+                line = l.decode().split()
+                word = line[0]
+                words.append(word)
+                word2idx[word] = i
+                i += 1
+                vect = np.array(line[1:]).astype(np.float)
+                vectors.append(vect)
+
+        self.vectors = np.asarray(vectors)
+        self.dictionary = word2idx
+        self.dim = self.vectors.shape[1]
+
+        print('Made an embedding containing ', len(words), 'words.')
+
+        return
+
+    def _argmax_top_k(self, x: np.array, k: int)-> (np.array, np.array):
+        copy = x
+        retv, reti = [], []
+        for repeat in range(k):
+            values = np.max(copy)
+            indices = np.where(copy == np.max(copy))
+            mask = np.arange(x.shape[1]).reshape(1, -1) == indices[1]
+            copy[mask] = -float('inf')
+            retv.append(values)
+            reti.append(indices[1])
+        return retv, reti
+
+    def _similarities(self, word_w: str) -> np.array:
+        w = self.dictionary[word_w]
+        vectors_t = np.transpose(self.vectors)
+
+        n_num = self.vectors[w] @ vectors_t
+        n_denom = np.linalg.norm(self.vectors[w], 2) * np.linalg.norm(vectors_t, 2)
+        sims = n_num / n_denom
+
+        return sims.reshape(1, len(self.dictionary))
+
+    def similarity(self, word_i: str, word_j: str) -> float:
+        i = self.dictionary[word_i]
+        j = self.dictionary[word_j]
+        v_i = self.vectors[i] / np.linalg.norm(self.vectors[i], 2)  # a/|a|
+        v_j = self.vectors[j] / np.linalg.norm(self.vectors[j], 2)  # b/|b|
+
+        return np.matmul(v_i.reshape(1, -1), v_j.reshape(-1, 1)).item()
+
+    def most_similar(self, word_i: str, k: int)-> List:
+        k += 1
+        sims = self._similarities(word_i)
+        _, topi = self._argmax_top_k(sims, k)
+        inv = {v: i for i, v in self.dictionary.items()}
+        return [inv[i[0]] for i in topi[1:k]]
+
+    def analogy(self, word_a: str, word_b: str, word_c: str, k: int) -> List:
+
+        v_d = self.vectors[self.dictionary[word_b]] - self.vectors[self.dictionary[word_a]] + self.vectors[self.dictionary[word_c]]
+        vectors_t = np.transpose(self.vectors)
+
+        n_t = v_d @ vectors_t
+        n_b = np.linalg.norm(v_d, 2) * np.linalg.norm(vectors_t, 2)
+        sims = n_t / n_b
+        sims = sims.reshape(1, len(self.dictionary))
+
+        _, topi = self._argmax_top_k(sims, k)
+        inv = {v: i for i, v in self.dictionary.items()}
+
+        return [inv[i[0]] for i in topi]
+
+    def get_word_vectors(self, text: List)-> np.array:
+        emb_dim = self.vectors.shape[1]
+        matrix_len = len(text)
+        weights_matrix = np.zeros((matrix_len, emb_dim))
+
+        for i, word in enumerate(text):
+            try:
+                weights_matrix[i] = self.vectors[self.dictionary[word]]
+            except KeyError:
+                weights_matrix[i] = np.zeros((emb_dim,))
+
+        return weights_matrix
 
 
-def make_sequences_set(text_set: List, seq_length: int, vectors: np.array, vocab: Dict[str, int], max_len: int,
-                       padding_val: float) -> (np.array, np.array):
-    seqs = []
-    targets = []
-    for text in text_set:
-        text_v = vectorize(text, vectors, vocab)
-        text_s, text_t = make_sequences(text_v, seq_length)
-        text_s = pad_sequences(text_s, max_len, value=padding_val, padding='pre', dtype='float')
-        targets.extend(text_t)
-        seqs.extend(text_s)
+class LSTMWordpred(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, n_layers):
+        super(LSTMWordpred, self).__init__()
 
-    x = np.array(seqs)
-    y = np.array(targets)
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
 
-    print("Amount of sequences: ", len(x))
+        # lstm layers
+        self.rnns = nn.ModuleList()
+        for i in range(n_layers):
+            input_size = self.embedding_dim if i == 0 else self.hidden_dim
+            self.rnns.append(nn.LSTM(input_size, self.hidden_dim, 1))
 
-    return x, y
+        # linear layer
+        self.dense_linear = nn.Linear(self.hidden_dim, self.embedding_dim)
 
+    def forward(self, sequence):
 
-def similarity(word_i: str, word_j: str, vocab: Dict[str,int], vectors: np.array) -> float:
-    i = vocab[word_i]
-    j = vocab[word_j]
-    v_i = vectors[i] / np.linalg.norm(vectors[i], 2)  # a/|a|
-    v_j = vectors[j] / np.linalg.norm(vectors[j], 2)  # b/|b|
+        # run sequences through all lstm layers
+        for i in range(len(self.rnns)):
+            if i != 0:
+                sequence = torch.nn.functional.dropout(sequence, p=0.2, training=True)
+            output, hidden = self.rnns[i](sequence)
+            sequence = output
 
-    return np.matmul(v_i.reshape(1, -1), v_j.reshape(-1, 1)).item()
+        lstm_out = sequence[:, -1, :]
 
+        out = self.dense_linear(lstm_out)
 
-def similarities(word_w: str, vocab: Dict[str,int], vectors: np.array)-> np.array:
-    w = vocab[word_w]
-    vectors_t = np.transpose(vectors)
+        return out
 
-    n_num = vectors[w] @ vectors_t
-    n_denom = np.linalg.norm(vectors[w], 2) * np.linalg.norm(vectors_t, 2)
-    sims = n_num / n_denom
+    def fit(self, dataset, n_epochs, val_split, batch_size, loss_function, opt, device='cpu'):
 
-    return sims.reshape(1, len(vocab))
+        # split dataset
+        val_size = int(val_split * len(dataset))
+        train_size = len(dataset) - val_size
+        train_set, val_set = random_split(dataset, [train_size, val_size])
 
+        # make dataloaders
+        train_gen = DataLoader(train_set, shuffle=True, num_workers=2, batch_size=batch_size)
+        val_gen = DataLoader(val_set, shuffle=True, num_workers=2, batch_size=batch_size)
 
-def most_similar(word_i: str, vocab: Dict[str,int], vectors: np.array, k: int)-> List:
-    k = k + 1
-    sims = similarities(word_i, vocab, vectors)
-    _, topi = argmax_top_k(sims, k)
-    inv = {v: i for i, v in vocab.items()}
-    return [inv[i[0]] for i in topi[1:k]]
+        train_hist = []
+        val_hist = []
 
+        print(f'Fitting model with {n_epochs} epochs over {len(dataset)} sequences.')
 
-def argmax_top_k(x: np.array, k: int)-> (np.array, np.array):
-    copy = x
-    retv, reti = [], []
-    for repeat in range(k):
-        values = np.max(copy)
-        indices = np.where(copy == np.max(copy))
-        mask = np.arange(x.shape[1]).reshape(1, -1) == indices[1]
-        copy[mask] = -float('inf')
-        retv.append(values)
-        reti.append(indices[1])
-    return retv, reti
+        for epoch in range(n_epochs):
+            train_loss = 0
+            val_loss = 0
+            i = 0
+            print(f'Starting epoch {epoch+1}')
+            for local_seq, local_tar in train_gen:
+                self.zero_grad()
 
+                local_seq = torch.squeeze(local_seq.float(), dim=1).to(device)
+                local_tar = torch.squeeze(local_tar.float(), dim=1).to(device)
 
-def analogy(word_a: str, word_b: str, word_c: str, vocab: Dict[str,int], vectors: np.array, k: int) -> List:
+                out = self(local_seq)
+                # print(out.shape)
 
-    v_d = vectors[vocab[word_b]] - vectors[vocab[word_a]] + vectors[vocab[word_c]]
-    vectors_t = np.transpose(vectors)
+                loss = loss_function(out, local_tar, torch.ones(self.embedding_dim, 1))
+                loss.backward()
+                opt.step()
 
-    n_t = v_d @ vectors_t
-    n_b = np.linalg.norm(v_d, 2) * np.linalg.norm(vectors_t, 2)
-    sims = n_t / n_b
-    sims = sims.reshape(1, len(vocab))
+                train_loss += loss.item()
+                i += 1
 
-    _, topi = argmax_top_k(sims, k)
-    inv = {v: i for i, v in vocab.items()}
+            train_loss = train_loss / i
+            train_hist.append(train_loss)
 
-    return [inv[i[0]] for i in topi]
+            with torch.no_grad():
+                i = 0
+                for local_seq, local_tar in val_gen:
+                    local_seq = torch.squeeze(local_seq.float(), dim=1).to(device)
+                    local_tar = torch.squeeze(local_tar.float(), dim=1).to(device)
 
-def build_context_rep_function(story_data: List, seq_length: int, vectors: np.array, vocab: Dict[str,int]) -> K.function:
-    max_seq_len = 20
-    PAD = 0.0
-    sequences, targets = make_sequences_set(story_data, seq_length, vectors, vocab, max_seq_len, PAD)
-    lstm_layer_size = 50
-    model = Sequential()
-    model.add(Masking(mask_value=PAD, input_shape=(max_seq_len, 50)))
-    model.add(LSTM(lstm_layer_size,
-                   dropout=0.2,
-                   input_shape=(max_seq_len, 50), recurrent_dropout=0.2, return_sequences=True, name='lstm1'))
+                    out = self(local_seq)
+                    loss = loss_function(out, local_tar, torch.ones(self.embedding_dim, 1))
+                    val_loss += loss.item()
+                    i += 1
 
-    model.add(LSTM(lstm_layer_size, dropout=0.2, recurrent_dropout=0.2, return_sequences=True, name='lstm2'))
-    model.add(LSTM(lstm_layer_size, name='lstm3'))
-    model.add(Dense(targets.shape[1], activation='linear'))
-    model.compile(loss='cosine_similarity', optimizer='adam', metrics=['cosine_similarity'])
+            val_loss = val_loss / i
+            val_hist.append(val_loss)
 
-    n_epochs = 50
-    model.fit(sequences,
-                targets,
-                epochs=n_epochs, batch_size=sequences.shape[0], verbose=1, validation_split=0.01)
+            print(f'Training loss: {train_loss}, Validation loss: {val_loss}')
 
-    return K.function([model.layers[0].input],
-                        [model.layers[3].output])
+        return train_hist, val_hist
+
+    def get_hidden_states(self, sequence: List)-> List:
+
+        # run sequence through all lstm layers
+        outputs = []
+        for i in range(len(self.rnns)):
+            if i != 0:
+                sequence = torch.nn.functional.dropout(sequence, p=0.2, training=False)
+            output, hidden = self.rnns[i](sequence)
+            sequence = output
+            outputs.append(output[:, -1, :])
+
+        return outputs
